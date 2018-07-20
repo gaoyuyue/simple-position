a2ec32dbd54256af25b6a05470a5e2ea8a064d18
高语越 <gaoyuyue@outlook.com>
2018-07-20 19:56:38 +0800

    更换过时接口

--- src/main/java/cn/attackme/simpleposition/config/WebSocketStompConfig.java
+++ src/main/java/cn/attackme/simpleposition/config/WebSocketStompConfig.java
@@ -2,9 +2,9 @@ package cn.attackme.simpleposition.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
 * @author 高语越 (Gao Yuyue)
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketStompConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();

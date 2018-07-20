ca3f4dfc9657714a2d95efc8acdf39c755f00f3f
高语越 <gaoyuyue@outlook.com>
2018-07-20 18:56:28 +0800

    Initial spring boot project

---
+++ src/main/java/cn/attackme/simpleposition/SimplePositionApplication.java
@@ -0,0 +1,12 @@
+package cn.attackme.simpleposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePositionApplication.class, args);
+	}
+}
